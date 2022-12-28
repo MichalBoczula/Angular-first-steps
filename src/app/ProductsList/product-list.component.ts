121ca7ae4c2f92d2103475d741435ab4215d4f8e
Michal Boczula <michal.boczula0101@gmail.com>
2022-12-28 20:29:10 +0100

    added service

--- src/app/ProductsList/product-list.component.ts
+++ src/app/ProductsList/product-list.component.ts
@@ -1,69 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
+  providers: [ProductService],
 })
-export class ProductList implements OnInit {
+export class ProductList implements OnInit, OnDestroy {
   title: string = 'new title';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
   filteredProducts: IProduct[] = [];
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2021',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: 'assets/images/leaf_rake.png',
-    },
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2021',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png',
-    },
-    {
-      productId: 5,
-      productName: 'Hammer',
-      productCode: 'TBX-0048',
-      releaseDate: 'May 21, 2021',
-      description: 'Curved claw steel hammer',
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl: 'assets/images/hammer.png',
-    },
-    {
-      productId: 8,
-      productName: 'Saw',
-      productCode: 'TBX-0022',
-      releaseDate: 'May 15, 2021',
-      description: '15-inch steel blade hand saw',
-      price: 11.55,
-      starRating: 3.7,
-      imageUrl: 'assets/images/saw.png',
-    },
-    {
-      productId: 10,
-      productName: 'Video Game Controller',
-      productCode: 'GMG-0042',
-      releaseDate: 'October 15, 2020',
-      description: 'Standard two-button video game controller',
-      price: 35.95,
-      starRating: 4.6,
-      imageUrl: 'assets/images/xbox-controller.png',
-    },
-  ];
+  private products: IProduct[] = [];
+  private errorMessage: string = '';
+  private subscribtion!: Subscription;
+
+  constructor(private productService: ProductService) { }
 
   private _listFilter: string = '';
   get listFilter(): string {
@@ -87,10 +43,15 @@ export class ProductList implements OnInit {
   }
 
   ngOnInit(): void {
-    this.listFilter = '';
+    this.products = this.productService.getProductsLocal()
+    this.filteredProducts = this.products;
   }
 
-  onRatingClicked(message: string) : void {
+  onRatingClicked(message: string): void {
     this.title = message;
+  }
+
+  ngOnDestroy(): void {
+    this.subscribtion.unsubscribe();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductList } from './ProductsList/product-list.component';
 import { ConvertToSpacePipe } from './ProductsList/product-list.pipe';
 import { Star } from './Star/star.component';
+import { ProductDetailsComponent } from './ProductDetails/product-details/product-details.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductList, ConvertToSpacePipe, Star],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, ProductList, ConvertToSpacePipe, Star, ProductDetailsComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
